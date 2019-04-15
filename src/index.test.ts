35e54928a19d45e95e840bb2d7327caeb915f5ca
Kjell Dießel <com.github@kettil.de>
2019-04-15 16:38:56 +0200

    Update index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
+import container from './container';
 import connect from './libs/connect';
+import Publisher from './libs/service/publisher';
+import Service from './libs/service/service';
+import Worker from './libs/service/worker';
 
-import index from './index';
+import index, * as indexObject from './index';
 
 describe('Check the index file', () => {
   /**
@@ -9,6 +13,10 @@ describe('Check the index file', () => {
   test('it should be return a connect when import default value', () => {
     expect(index).toBe(connect);
 
-    expect(true).toBeTruthy();
+    expect(indexObject.container).toBe(container);
+
+    expect(indexObject.AbstractService).toBe(Service);
+    expect(indexObject.PublisherService).toBe(Publisher);
+    expect(indexObject.WorkerService).toBe(Worker);
   });
 });
