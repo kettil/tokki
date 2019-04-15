004d5e2609eba6e232f794e5460672561d4f7378
Kjell Dießel <com.github@kettil.de>
2019-04-15 16:38:30 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import _container from './container';
+import _Publisher from './libs/service/publisher';
+import _Service from './libs/service/service';
+import _Worker from './libs/service/worker';
+
 import connect from './libs/connect';
 
 export * from './libs/types';
 
 export default connect;
+
+export const container = _container;
+
+export const AbstractService = _Service;
+export const PublisherService = _Publisher;
+export const WorkerService = _Worker;
