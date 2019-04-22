1c4fcbb9092b7e03ef6dffede31a4fad30890116
Kettil <development@kettil.de>
2019-04-22 15:07:49 +0200

    Add code coverage

--- jest.in.js
+++ jest.in.js
@@ -1,6 +1,22 @@
 // expand jest config for ci
 var jest = {
   roots: ['<rootDir>/tests/integration/'],
+
+  // enabled coverage
+  collectCoverage: true,
+  // only text coverage
+  coverageReporters: ['text-summary'],
+  // no output
+  coverageDirectory: '',
+
+  coverageThreshold: {
+    global: {
+      branches: 25,
+      functions: 25,
+      lines: 25,
+      statements: 25,
+    },
+  },
 };
 
 // export modified jest config
